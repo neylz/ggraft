983b2910d0afddab71eb39821c346a71da2e0b6e
Neylz <pacome@neylz.dev>
2026-09-18 14:29:11 +0200

    CD & dyn versionning

--- src/ggraft/__init__.py
+++ src/ggraft/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("ggraft")
+except PackageNotFoundError:   # a source tree that was never installed
+    __version__ = "0.0.0"
